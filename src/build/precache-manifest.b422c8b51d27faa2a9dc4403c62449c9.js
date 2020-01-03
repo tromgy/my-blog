cb5b3ac9be237acd3cb24e91bf0e26a48333403f
tromgy <tromgy@yahoo.com>
2020-01-03 11:18:22 -0500

    Initial commit

---
+++ src/build/precache-manifest.b422c8b51d27faa2a9dc4403c62449c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d219cdfdfe353c4792319585da4767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85472fc9d85abf7f21bd",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a83ce300e0de56d9407d",
+    "url": "/static/js/2.ab57f58e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ab57f58e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85472fc9d85abf7f21bd",
+    "url": "/static/js/main.b67a0ccb.chunk.js"
+  },
+  {
+    "revision": "6e55ea343e46b8691e3d",
+    "url": "/static/js/runtime-main.b5470c2c.js"
+  }
+]);
